888926538b5607161f03b1a1a1106ad607788871
mominriyadh <riyadh@codesign.com.bd>
2020-08-26 20:51:42 +0600

    fix and deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="container-fluid">
             <div className="jumbotron">
-                <h1 className="display-4 text-center">Redmi k40 ultra</h1>
+                <h1 className="display-4 text-center">Redmi k30 ultra</h1>
             </div>
         </div>
     )
